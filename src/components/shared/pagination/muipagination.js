9d761442dc54c5f15b8bdd11a6458f92fae90fb3
Ali Mohamed <amohamed@onesolutionc.com>
2023-03-10 22:06:05 +0200

    responsive pagination component

--- src/components/shared/pagination/muipagination.js
+++ src/components/shared/pagination/muipagination.js
@@ -2,13 +2,26 @@ import { Pagination } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import PaginationItem from '@mui/material/PaginationItem';
-import styles from './pagination.module.css'
+import styles from './pagination.module.css';
+import{useState,useEffect}from 'react'
 
 const Muipagination = ({ count, handleChange,pageNumber}) => {
-
+const [paginationSize, setPaginationSize] = useState('large');
+function handleResize() {
+  if(window.innerWidth>700){
+    setPaginationSize('medium');
+}else{
+  setPaginationSize('small');
+  }
+}
+// ------------------------------------------------------------------------------------------------
+useEffect(() => {
+  window.addEventListener('resize', handleResize);
+}, []);
   return (<div >
     <div className={styles.padination_wrapper} >
       <Pagination
+      size={paginationSize}
         shape="rounded"
         count={count} page={pageNumber} onChange={(e, value) => {
           handleChange(value);
